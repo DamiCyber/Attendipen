9e1a70acd7b2d74e298b6b5c3da57a871b79797c
damola ifatifo <ifatifod4@gmail.com>
2025-04-17 22:17:02 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import tailwindcss from '@tailwindcss/vite'
-import flowbiteReact from "flowbite-react/plugin/vite";
-import autoprefixer from 'autoprefixer'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer,
-      ],
-    },
-  },
+  plugins: [react()],
 })
